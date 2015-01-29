1404eb30e8ee11ffe49af54f56da675210449def
Madi Alexander <madi.alexander@gmail.com>
2015-01-29 10:17:46 -0600

    update
    updates

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
+
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
+function getData() {
+  $.getJSON("carlosgomez.json", function(data, error) {
+    playerData = data;
+    drawStuff();
+  });
 
+    function drawStuff() {
 
+      var playerName = playerData.name;
+      var playerClub = playerData.club;
 
+      var playerStat = playerData.stats;
+      var lastSeason = playerStat.length -1;
 
+      var lastSeasonstats = playerData.stats[lastSeason];
 
+      var seasonAtBats = lastSeasonstats.AB;
+      var seasonYear = lastSeasonstats.year;
+      var seasonClub = lastSeasonstats.club;
+
+        $(".chart").append("<h2>"+playerName+"</h2>");
+        $(".chart").append("<h3>"+playerClub+"</h3>");
+        $(".chart").append("<p>"+playerName+" had "+seasonAtBats+" at bats for the "+seasonClub+" in "+seasonYear+".</p>");
+
+  }
+
+};
